cc8ab96372ee4e57c271d291c0e5aea80b410a49
Paola de Andrade <ppes1@student.monash.edu>
2021-08-08 19:30:24 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const form=document.getElementById('form')
+
+form.addEventListener('submit', (e)  => {
+    //e.preventDefault();
+    //alert("Cadastrado com sucesso!")
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    console.log("nome = " + nome)
+    console.log("email = " + email)
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+
+    let carregando = `<p>Carregando...<\p>`
+
+    let pronto = `<p>Pronto, você foi cadastrado!<\p>`
+
+    content.innerHTML = carregando
+
+    setTimeout(() => {
+        content.innerHTML = pronto
+},1000)
+
+})

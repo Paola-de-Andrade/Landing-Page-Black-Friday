f430c7af72792a41139c789e4b56c6365dc484a4
Paola de Andrade <ppes1@student.monash.edu>
2021-08-09 13:54:05 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const form=document.getElementById('form')
 
 form.addEventListener('submit', (e)  => {
-    //e.preventDefault();
+    e.preventDefault();
     //alert("Cadastrado com sucesso!")
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
